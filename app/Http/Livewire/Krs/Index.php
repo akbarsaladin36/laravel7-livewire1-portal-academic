c79ca0d73497f9e1b5d4b5a2f8d8f1cd0849a2e9
akbarsaladin36 <asong36@gmail.com>
2024-04-17 21:28:03 +0700

    adding new function for delete kartu-rencana-studi data

--- app/Http/Livewire/Krs/Index.php
+++ app/Http/Livewire/Krs/Index.php
@@ -3,10 +3,24 @@
 namespace App\Http\Livewire\Krs;
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 use Livewire\Component;
 
 class Index extends Component
 {
+
+    public function delete($krs_id)
+    {
+        $krs = DB::table('pa_krs')->where('krs_id',$krs_id);
+
+        if($krs) {
+            $krs->delete();
+            Session::flash('success','Data krs ini sudah berhasil dihapus!');
+            return redirect()->back();
+        }
+    }
+
+
     public function render()
     {
         $krss = DB::table('pa_krs')
--- resources/views/livewire/krs/index.blade.php
+++ resources/views/livewire/krs/index.blade.php
@@ -5,6 +5,13 @@
         <h2>List Semua KRS</h2>
     </div>
 
+    @if(Session::get('success'))
+        <div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+
     <div class="mt-3">
         <div class="mt-3 d-flex justify-content-end">
             <a href="{{ route('krs.create') }}" class="btn btn-primary">Tambah KRS</a>
@@ -35,7 +42,7 @@
                         <td>{{ $krs->sks }}</td>
                         <td>
                             <a href="{{ route('krs.detail',['krs_id'=>$krs->krs_id]) }}" class="btn btn-success">Detail</a>
-                            <button class="btn btn-danger">Delete</button>
+                            <button wire:click="delete({{ $krs->krs_id }})" class="btn btn-danger">Delete</button>
                         </td>
                     </tr>
                 @endforeach
